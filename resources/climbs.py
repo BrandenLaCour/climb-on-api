621acc2b213e8ca48c6bca6c2e56fa877e4788fd
brandenlacour <brandenlacour@gmail.com>
2020-02-18 12:16:36 -0600

    can update climb

--- resources/climbs.py
+++ resources/climbs.py
@@ -15,6 +15,7 @@ def all_climbs():
 
 #create route
 @climbs.route('/', methods=['POST'])
+@login_required
 def create_climb():
     payload = request.get_json()
     payload = {**payload, "user": current_user.id}
@@ -22,3 +23,18 @@ def create_climb():
     created_climb_dict = model_to_dict(created_climb)
     created_climb_dict['user'].pop('password')
     return jsonify(data=created_climb_dict, message='successfully created climb {}'.format(created_climb_dict['name']), status=200), 200
+
+@climbs.route('/<id>', methods=['PUT'])
+@login_required
+def update_climb(id):
+    payload = request.get_json()
+    try:
+        models.Climb.get(models.Climb.user == current_user.id)
+        update_query = models.Climb.update(**payload).where(models.Climb.id == id)
+        update_query.execute()
+        updated_climb = models.Climb.get_by_id(id)
+        updated_climb_dict = model_to_dict(updated_climb)
+        return jsonify(data=updated_climb_dict, message='successfully update the climb {}'.format(updated_climb_dict['name']), status=200),200
+
+    except models.DoesNotExist:
+        return jsonify(data={}, message='cannot update climb, is not connected to user', status=200), 200
